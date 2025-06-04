25c6411114d199e1dfb5da761f8d3a9d1052f3c4
Felipe Pegoraro <felipegoraro@outlook.com.br>
2025-06-04 09:46:40 -0300

    carregando mensagens unica do kafka

--- src/inbound/consumidor.py
+++ src/inbound/consumidor.py
@@ -34,17 +34,60 @@ def carregar_dados():
     # Definindo a coleção no MongoDB
     collection = db[data_atual]
 
+    def listar_valores_collection():
+
+        dados_colection = []
+
+        documentos = collection.find()
+        for documento in documentos:
+            linha = {
+                "_id": documento.get("_id"),
+                "PreTaxCost": documento.get("PreTaxCost"),
+                "UsageDate": documento.get("UsageDate"),
+                "ResourceGroup": documento.get("ResourceGroup"),
+                "ResourceId": documento.get("ResourceId"),
+                "Currency": documento.get("Currency")
+            }
+            dados_colection.append(linha)
+
+        return dados_colection
+    valors_collection = listar_valores_collection()
+
+
+
     print("Aguardando a última mensagem do Kafka...")
     for message in consumer:
         # Obtém apenas a mensagem mais recente que chega
         data = message.value
         print("Última mensagem recebida:", data)
 
+        # Verifica se os valores de data já existem em valors_collection
+        def existe_nos_valores(dado, valores):
+            for valor in valores:
+                if (
+                    dado.get("PreTaxCost") == valor.get("PreTaxCost") and
+                    dado.get("UsageDate") == valor.get("UsageDate") and
+                    dado.get("ResourceGroup") == valor.get("ResourceGroup") and
+                    dado.get("ResourceId") == valor.get("ResourceId") and
+                    dado.get("Currency") == valor.get("Currency")
+                ):
+                    return True
+            return False
+
+        if isinstance(data, list):
+            novos_dados = [d for d in data if not existe_nos_valores(d, valors_collection)]
+        else:
+            novos_dados = [data] if not existe_nos_valores(data, valors_collection) else []
+
+        if not novos_dados:
+            print("Nenhum novo dado para inserir. Todos já existem na collection.")
+            continue
+
         try:
-            if isinstance(data, list):  # Se o JSON for uma lista de objetos
-                collection.insert_many(data)
+            if isinstance(novos_dados, list):  # Se o JSON for uma lista de objetos
+                collection.insert_many(novos_dados)
             else:  # Se o JSON for um único objeto
-                collection.insert_one(data)
-            print(f"Dados salvos na collection {collection} do MongoDB:", data)
+                collection.insert_one(novos_dados)
+            print(f"Dados salvos na collection {collection} do MongoDB:", novos_dados)
         except Exception as e:
             print("Erro ao salvar no MongoDB:", e)
